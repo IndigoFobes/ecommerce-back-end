d7c0c3719da20866a9fc478e7b2ebc9b532a70fd
Indigo <indigo.crandell@gmail.com>
2022-10-03 14:31:40 -0400

    Added tag routes.

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,9 +7,12 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
   } catch (err) {
-
+    res.status(400).json(err);
   }
 });
 
@@ -17,36 +20,57 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
   } catch (err) {
-    
+    res.status(400).json(err);
   }
 });
 
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
-    
+    res.status(404).json(err);
   }
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+    );
+    res.status(200).json(tagData);
   } catch (err) {
-    
+    res.status(404).json(err);
   }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-
+    const tagData = await Tag.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(tagData);
   } catch (err) {
-    
+    res.status(400).json(err);
   }
 });
 
